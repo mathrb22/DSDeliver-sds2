25e91e672d3254198d61c7a1a5b409fa5656ed20
Matheus Ribeiro <mathribe2020@gmail.com>
2021-01-06 17:24:05 -0300

    Implementação da camada de serviço de Product e seu DTO

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -0,0 +1,79 @@
+package com.devsuperior.dsdeliver.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+public class ProductDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+	
+	public ProductDTO() {
+		
+	}
+
+	public ProductDTO(Long id, String name, Double price, String description, String imageUri) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+	
+	//Constructor using the Product entity as param:
+	public ProductDTO(Product entity) {
+		id = entity.getId();
+		name = entity.getName();
+		price = entity.getPrice();
+		description = entity.getDescription();
+		imageUri = entity.getImageUri();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+	
+	
+}
---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll() {
+		List<Product> list = repository.findAll();
+		return list.stream().map(element -> new ProductDTO(element)).collect(Collectors.toList());
+	}
+}
