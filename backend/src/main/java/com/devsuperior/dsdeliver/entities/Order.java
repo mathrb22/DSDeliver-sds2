c8b6467e597c3f85fca861d4ce84c23f1a44267f
Matheus Ribeiro <mathribe2020@gmail.com>
2021-01-06 15:16:46 -0300

    Mapeamento objeto-relacional das entidades Order e Product

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -5,10 +5,23 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String address;
 	private Double latitude;
@@ -16,6 +29,11 @@ public class Order implements Serializable{
 	private Instant moment;
 	private OrderStatus status;
 	
+	@ManyToMany
+	@JoinTable(
+		name = "tb_order_product",
+		joinColumns = @JoinColumn(name = "order_id"),
+		inverseJoinColumns = @JoinColumn(name = "product_id"))
 	private Set<Product> products = new HashSet<>();
 
 	public Order() {
--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -2,10 +2,20 @@ package com.devsuperior.dsdeliver.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
 public class Product implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double price;
