b96838f996f6689593537730c0e529accef4fea1
Matheus Ribeiro <mathribe2020@gmail.com>
2021-01-07 15:29:11 -0300

    Limpeza dos arquivos iniciais do projeto

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Hello World</h2>
+			<h3>DSDeliver - Semana DevSuperior 2.0</h3>
+		</div>
+	);
 }
 
 export default App;
